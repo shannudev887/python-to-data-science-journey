de59dab51e4184c9bf0ec694502f1b09d39ebd53
shannudev887 <sunilchowdary887@gmail.com>
2025-11-19 19:50:19 +0530

    day 5

---
+++ day5/controlflow.py
@@ -0,0 +1,47 @@
+# x = "shannu"
+# y = [1,2,3,4]
+# if 3 not in y:
+#     print("not an int")
+# elif type(x) is list:
+#     print("its a list")
+# else:
+#     print("Bitch")
+    
+# x = 1    
+# a = x=3 if x < 2 else x == 1
+# a = x==3 if x < 2 else x == 1
+# print(a)
+
+# f = ["s","h","a"]
+# s = "shannu"
+# for i in range(len(s)):
+#     if i:
+#         pass
+#     if s[i] == "a":
+#         continue
+#     print(s[i])
+#     if i== 2:
+#         break
+# else:
+#     print("Done with the iterations!!")
+    
+# x=0
+# while True:
+
+#     x += 1
+#     if x==2:
+#         continue
+#     if x==4:
+#         break
+#     print(x)
+    
+# representation of the do while loop 
+#we can do or execute a operation and then we will check the condition for the next iteration
+x=0
+while True:
+    if x ==2: 
+        break
+    x+=1
+    print(x)
+    
+    
